6770da158d9af8df5c07f3954a622ff684c86a10
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-05-08 16:09:05 +0200

    ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
-
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { ColorSchemeScript } from '@mantine/core';
 //Screens
 import DeaScreen from './DeaScreen';
 
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 function App() {
   return (
-    <DeaScreen />
+    <MantineProvider theme={theme}>
+      <DeaScreen />
+    </MantineProvider>
   );
 }
 
